d1a075b33e6e1cba434da2cd26334b2072865a7f
Vitaly Vasin <neural.null@gmail.com>
2017-12-26 21:08:33 +0300

    optimize node creation

--- graph.js
+++ graph.js
@@ -1,13 +1,13 @@
 'use strict'
 
-function Node(graph, func) {
+function Node(func) {
     var f = function() {
         if (!f.evaluated) {
             if (f.evaluating) {
                 throw 'Cyclic graphs are not allowed';
             }
             f.evaluating = true;
-            f.value = func(graph);
+            f.value = func();
             f.evaluating = false;
             f.evaluated = true;
         }
@@ -17,6 +17,7 @@ function Node(graph, func) {
     f.evaluating = false;
     f.evaluated = false;
     f.value = undefined;
+    f.reset = function() { f.evaluated = false; };
 
     return f;
 }
@@ -27,16 +28,16 @@ function Graph(graph) {
         if (typeof value == 'function') {
             let func;
             try {
-                func = new Function('__graph__', `with (__graph__) return (function ${value})();`);
+                func = new Function('__graph__', `with (__graph__) return function ${value};`)(this);
             } catch (e) {
                 try {
-                    func = new Function('__graph__', `with (__graph__) return (${value})();`);
+                    func = new Function('__graph__', `with (__graph__) return ${value};`)(this);
                 } catch (e) {
                     func = value;
                 }
             }
             Object.defineProperty(this, key, {
-                get: Node(this, func)
+                get: Node(func)
             });
         } else {
             Object.defineProperty(this, key, {
